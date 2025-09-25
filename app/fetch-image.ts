6a84c0a01bf904aaa45cf460c71b037328433fea
iHok <ihokya@gmail.com>
2025-09-25 17:27:20 +0900

    vercelで Promese<Image>　で怒られ他っぽいので、とりあえず削除

--- app/fetch-image.ts
+++ app/fetch-image.ts
@@ -6,7 +6,7 @@ type Image = {
     url: string;
 }
 
-export async function fetchImage(): Promese<Image> {
+export async function fetchImage() {
   const res = await fetch("https://api.thecatapi.com/v1/images/search", {
     headers: { "x-api-key": CAT_API_KEY },
   });
